7ddb6c95b58496835ec5150e5696dbbc72bfe081
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-06-13 15:52:24 +0100

    move creds to server

---
+++ creds.py
@@ -0,0 +1,13 @@
+creds = {
+    "TMDB": {
+        "access_token": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmExNTAzOWNlNjRlZjRmY2FkM2NkNTg0MmNhZDdhNSIsInN1YiI6IjY1ZjQ4MjI0MDg4YzZiMDE3ZDA0OTM0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.68Y3xL5Vlj7g22rA0_sPpUnYI2juPpws8Sk26lPw0dc",
+        "key": "92a15039ce64ef4fcad3cd5842cad7a5",
+    },
+    "RAWG": {"key": "2381793d86da45e4be94e25dcc60ec61"},
+}
+
+endpoints = {
+    "tmdb": "https://api.themoviedb.org/3/",
+    "tmdbImage": "http://image.tmdb.org/t/p/",
+    "rawg": "https://api.rawg.io/api/",
+}
